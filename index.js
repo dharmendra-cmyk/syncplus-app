c41f3fe3cdbc6f26ecffba729cdf865acdd59107
dharmendra-cmyk <dharmendra@ahluwaliastrategic.company>
2026-08-31 17:54:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.post('/create-checkout-session', async (req, res) => {
           quantity: 1,
         },
       ],
+      customer_creation: 'if_required',
       success_url: `${req.protocol}://${req.get('host')}/success.html?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.protocol}://${req.get('host')}/cancel.html`,
     });
