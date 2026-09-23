011d28fc8a8b875fa5e3c347146ebb813bbd0ef4
dharmendra-cmyk <dharmendra@ahluwaliastrategic.company>
2026-09-23 12:28:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
+// Root route to serve your interactive UI and satisfy Shopify app review requirement 2.1.3
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', { root: '.' });
+});
+
 // Mandatory Shopify GDPR Compliance Webhooks
 app.post('/api/webhooks', async (req, res) => {
   const topic = req.get('X-Shopify-Topic');
   const shop = req.get('X-Shopify-Shop-Domain');
-  
+
   console.log(`Received Shopify webhook topic: ${topic} for shop: ${shop}`);
   res.status(200).send({ success: true });
 });
